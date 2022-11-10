10e2eff5c86eeb8866de15eba00ddb8e96d394ec
jspoh <jingsengyeah@gmail.com>
2022-11-11 02:12:20 +0800

    created modal component

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,8 +3,31 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  styleUrls: ['./app.component.scss'],
 })
 export class AppComponent {
   title = 'todo';
+
+  /* template for modal content */
+  modal_content = {
+    header: 'new todo item',
+    inputs: [
+      {
+        label: 'title',
+      },
+    ],
+  };
+
+  /* dummy data */
+  todo_list = [
+    {
+      title: 'Get groceries',
+    },
+    {
+      title: 'Pay bills',
+    },
+    {
+      title: 'Check news',
+    },
+  ];
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,21 @@
+import { CommonModule } from '@angular/common';
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { ModalModule } from './shared/components/modal/modal.module';
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
+  declarations: [AppComponent],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    CommonModule,
+    BrowserAnimationsModule,
+    ModalModule,
   ],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
